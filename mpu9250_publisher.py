515b7370fea158d361047a5dec3955d5586206d7
SWJeon43 <jswkiss88@gmail.com>
2024-06-24 17:34:02 +0900

    added location logic

--- mpu9250_publisher.py
+++ mpu9250_publisher.py
@@ -5,6 +5,7 @@ from tf2_ros import TransformBroadcaster
 from geometry_msgs.msg import TransformStamped
 import smbus2
 import math
+import numpy as np
 
 class MPU9250Publisher(Node):
     def __init__(self):
@@ -16,6 +17,9 @@ class MPU9250Publisher(Node):
         self.bus = smbus2.SMBus(1)
         self.address = 0x68
         self.bus.write_byte_data(self.address, 0x6B, 0)
+        self.position = np.zeros(3)
+        self.velocity = np.zeros(3)
+        self.last_time = self.get_clock().now()
 
     def read_i2c_word(self, reg):
         high = self.bus.read_byte_data(self.address, reg)
@@ -26,28 +30,40 @@ class MPU9250Publisher(Node):
         return value
 
     def timer_callback(self):
-        accel_x = self.read_i2c_word(0x3B)
-        accel_y = self.read_i2c_word(0x3D)
-        accel_z = self.read_i2c_word(0x3F)
-        gyro_x = self.read_i2c_word(0x43)
-        gyro_y = self.read_i2c_word(0x45)
-        gyro_z = self.read_i2c_word(0x47)
+        current_time = self.get_clock().now()
+        dt = (current_time - self.last_time).nanoseconds / 1e9
+        self.last_time = current_time
+
+        accel_x = self.read_i2c_word(0x3B) / 16384.0
+        accel_y = self.read_i2c_word(0x3D) / 16384.0
+        accel_z = self.read_i2c_word(0x3F) / 16384.0
+        gyro_x = self.read_i2c_word(0x43) / 131.0
+        gyro_y = self.read_i2c_word(0x45) / 131.0
+        gyro_z = self.read_i2c_word(0x47) / 131.0
+
+        # 중력 가속도 제거 (필요에 따라 적용)
+        accel_z -= 1.0
 
         imu_msg = Imu()
         imu_msg.header.stamp = self.get_clock().now().to_msg()
         imu_msg.header.frame_id = 'imu_link'
 
-        imu_msg.linear_acceleration.x = accel_x / 16384.0
-        imu_msg.linear_acceleration.y = accel_y / 16384.0
-        imu_msg.linear_acceleration.z = accel_z / 16384.0
+        imu_msg.linear_acceleration.x = accel_x
+        imu_msg.linear_acceleration.y = accel_y
+        imu_msg.linear_acceleration.z = accel_z
 
-        imu_msg.angular_velocity.x = gyro_x / 131.0
-        imu_msg.angular_velocity.y = gyro_y / 131.0
-        imu_msg.angular_velocity.z = gyro_z / 131.0
+        imu_msg.angular_velocity.x = gyro_x
+        imu_msg.angular_velocity.y = gyro_y
+        imu_msg.angular_velocity.z = gyro_z
 
         self.publisher_.publish(imu_msg)
 
-        # IMU 데이터를 사용하여 회전 변환 계산
+        # 속도 및 위치 업데이트
+        acceleration = np.array([accel_x, accel_y, accel_z])
+        self.velocity += acceleration * dt
+        self.position += self.velocity * dt
+
+        # 회전 변환 계산
         roll = math.atan2(accel_y, accel_z)
         pitch = math.atan2(-accel_x, math.sqrt(accel_y * accel_y + accel_z * accel_z))
         yaw = 0.0  # Yaw는 자이로스코프 데이터를 적분하여 계산해야 하지만 여기서는 생략합니다.
@@ -67,11 +83,11 @@ class MPU9250Publisher(Node):
 
         t = TransformStamped()
         t.header.stamp = self.get_clock().now().to_msg()
-        t.header.frame_id = 'base_link'
+        t.header.frame_id = 'world'
         t.child_frame_id = 'imu_link'
-        t.transform.translation.x = 0.0
-        t.transform.translation.y = 0.0
-        t.transform.translation.z = 0.0
+        t.transform.translation.x = self.position[0]
+        t.transform.translation.y = self.position[1]
+        t.transform.translation.z = self.position[2]
         t.transform.rotation.x = qx
         t.transform.rotation.y = qy
         t.transform.rotation.z = qz
